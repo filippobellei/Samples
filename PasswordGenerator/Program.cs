cf8bb86f28626bc5afd8779b763d3f80cd7de37f
Filippo Bellei <filippobellei@gmail.com>
2024-05-31 09:49:21 +0200

    feat: add PasswordGenerator

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+Console.Write("Bytes lenght: ");
+var length = Convert.ToInt32(Console.ReadLine());
+
+var bytes = RandomNumberGenerator.GetBytes(length);
+var password = Convert.ToBase64String(bytes);
+
+Console.WriteLine(password);
