396807eb544f87b027afe9ff5e59071351d2bff8
Filippo Bellei <f.bellei@ingferrari.it>
2025-06-11 10:01:19 +0200

    feat: add TypeScript to StreamSample

--- StreamSample/wwwroot/script.ts
+++ StreamSample/wwwroot/script.ts
@@ -1,8 +1,10 @@
+export { };
+
 const paragraph = document.getElementById('paragraph');
 const decoder = new TextDecoder();
 const response = await fetch('streamTextSimulator');
 
-for await (const chunk of response.body) {
+for await (const chunk of response.body!) {
     const text = decoder.decode(chunk).split('"')[1];
-    paragraph.innerHTML += text;
+    paragraph!.innerHTML += text;
 }
