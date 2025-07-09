7cf1ec96153d7429857ada3a541f2a5ca572cf37
Filippo Bellei <f.bellei@ingferrari.it>
2025-07-09 15:38:35 +0200

    fix: remove unused Controllers config from StreamSample project

--- StreamSample/Program.cs
+++ StreamSample/Program.cs
@@ -2,12 +2,9 @@ using StreamSample.Endpoints;
 
 var builder = WebApplication.CreateBuilder();
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
 
 app.UseFileServer();
-app.MapControllers();
 app.MapStreamEndpoints();
 
 app.Run();
